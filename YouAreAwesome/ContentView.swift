8fa5be3416d85f6f70dffe3dff52a6eafcf4727a
rbarsk <rbarsk@gmail.com>
2024-10-16 21:35:48 +0100

    Ch. 1.5 Getting SwiftUI Layout Right Part I - Borders, Frames, multiline alignment (You Are Awesome)

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,14 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var messageString = ""
+    @State private var messageString = "When the Genius Bar needs help, they call you"
     
     var body: some View {
-        VStack {
+    
+       
+        VStack (spacing: 20){
+            
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
                 .foregroundColor(.red)
+                
+                .frame( height:150)
+                .frame(maxWidth:.infinity)
+                .border(.orange ,width:1)
                 .padding()
             
             HStack{
@@ -31,9 +40,12 @@ struct ContentView: View {
                 }
                 .buttonStyle(.borderedProminent)
             }
+            .border(.purple ,width:5)
+            
         }
     }
-}
+    }
+
 
 #Preview {
     ContentView()
